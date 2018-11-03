fa5bb403c99e7b9b921941158b66ee750a265651
skw13a <skw13a@acu.edu>
2018-11-03 16:26:08 -0500

    Updating DB schema.

--- CARIS-db-test.sql
+++ CARIS-db-test.sql
@@ -14,30 +14,20 @@ create table members -- keeps track of all members who might attend sessions. In
     age int unsigned,
     picture varchar(255),
     Notes varchar(511),
+    timesVisited int unsigned,
     PRIMARY KEY (ID)
-);
-
-create table lessons -- keeps track of the different lessons that users can teach.
-(
-	lessonID int unsigned NOT NULL,
-    lessonName varchar(255),
-    PRIMARY KEY (lessonID)
-);
-
-create table sessions -- keeps track of all individual meetings where lessons are taught. Is linked to lessons so that we can tell what lesson is being taught at the session.
-(
-	sessionID int unsigned NOT NULL,
-    lesson int unsigned NOT NULL,
-    sessionDate datetime, -- format: YYYY-MM-DD HH:MI:SS
-    sessionLocation varchar(255),
-    PRIMARY KEY (lesson, sessionDate, sessionLocation), -- this allows multiple sessions to occur at the same time in different places, or in the same place at different times.
-    CONSTRAINT FOREIGN KEY (lesson) REFERENCES lessons(lessonID)
 );
 
 create table attendeeList -- Keeps track of all members attending any given session.
 (
 	member int unsigned NOT NULL,
-    sessionAttended int unsigned NOT NULL,
-    PRIMARY KEY (member, sessionAttended),
+    dateAttended datetime NOT NULL,
+    PRIMARY KEY (member, dateAttended),
     CONSTRAINT FOREIGN KEY (member) REFERENCES members(ID)
+);
+
+create table benefits -- keeps track of all individual meetings where lessons are taught. Is linked to lessons so that we can tell what lesson is being taught at the session.
+(
+	resetTime datetime,
+    doesReset int(1) NOT NULL
 );
