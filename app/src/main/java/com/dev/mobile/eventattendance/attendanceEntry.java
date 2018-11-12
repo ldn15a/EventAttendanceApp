2dd07ea6b7b7768e645360aeb4562025768b33c5
skw13a <skw13a@acu.edu>
2018-11-12 15:31:45 -0600

    db changes prior to large update.

--- app/src/main/java/com/dev/mobile/eventattendance/attendanceEntry.java
+++ app/src/main/java/com/dev/mobile/eventattendance/attendanceEntry.java
@@ -1,11 +1,8 @@
 package com.dev.mobile.eventattendance;
 
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
-
-import android.support.annotation.NonNull;
 
 @Entity(foreignKeys = @ForeignKey(entity = Member.class, parentColumns = "ID", childColumns = "member"))
 public class attendanceEntry {
