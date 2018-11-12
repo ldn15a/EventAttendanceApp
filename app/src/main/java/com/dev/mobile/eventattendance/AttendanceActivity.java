5664abb6ff49d1a09d6293356dfcdbf612e3192b
skw13a <skw13a@acu.edu>
2018-11-12 16:00:50 -0600

    large DB update. All current errors resolved and ready for merge.

--- app/src/main/java/com/dev/mobile/eventattendance/AttendanceActivity.java
+++ app/src/main/java/com/dev/mobile/eventattendance/AttendanceActivity.java
@@ -1,15 +1,17 @@
 package com.dev.mobile.eventattendance;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
