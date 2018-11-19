a702478d3d9fc859de2e95f3ced28cfcb0bdd09d
skw13a <skw13a@acu.edu>
2018-11-19 04:40:26 -0600

    DB Update - merge complete and DB linked with Attendance Page. Search bar and image functions still being finalized.

--- app/src/main/java/com/dev/mobile/eventattendance/appDatabase.java
+++ app/src/main/java/com/dev/mobile/eventattendance/appDatabase.java
@@ -1,11 +1,30 @@
 package com.dev.mobile.eventattendance;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Member.class, attendanceEntry.class, benefitsData.class}, version = 1, exportSchema = false)
-public abstract class appDatabase extends RoomDatabase{
+@Database(entities = {Member.class, AttendanceEntry.class, benefitsData.class, Lesson.class}, version = 2, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase{
+
+    private static AppDatabase INSTANCE;
 
     public abstract dbInterface dbInterface();
 
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "EventAttendanceDB")
+                            .allowMainThreadQueries()
+                            .fallbackToDestructiveMigration()
+                            .build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
